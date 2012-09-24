542422f4f77a8322ac6d0fbe1c61db01997116b5
Iain Maguire <iainmaguire@Macintosh-2.local>
2012-09-23 23:19:50 -0400

    Initial Commit

---
+++ Hungry Fish/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Hungry Fish
+//
+//  Created by Iain Maguire on 23/09/2012.
+//  Copyright Personal Projects 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Hungry Fish/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hungry Fish
+//
+//  Created by Iain Maguire on 23/09/2012.
+//  Copyright Personal Projects 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
